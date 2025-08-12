d36f636e07d07e109e5e3692a3a5f96f74e2cb5b
radch <radchenko.daniil2471@gmail.com>
2025-08-12 22:54:49 +0300

    update and start files

--- updateWin.bat
+++ updateWin.bat
@@ -1,3 +1,6 @@
 git init
+git fetch
+git reset --hard HEAD
+git merge origin/$CURRENT_BRANCH
 git pull https://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@github.com/den2471/tgSellBot
 exit
