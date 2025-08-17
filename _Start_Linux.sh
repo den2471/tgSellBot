f31845967bd4a671df229cb155071dcd63ce05f1
radch <radchenko.daniil2471@gmail.com>
2025-08-17 17:09:45 +0300

    linux startup fix

--- _Start_Linux.sh
+++ _Start_Linux.sh
@@ -4,7 +4,7 @@ VENV_DIR=".venv"
 REQUIREMENTS="requirements.txt"
 
 apt-get update
-pip install --upgrade pip
+pip install --upgrade pip --root-user-action
 apt install git -y
 
 echo "Checking updates"
