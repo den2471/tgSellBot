186540ed1911ff931af961983fed15465f8a5059
radch <radchenko.daniil2471@gmail.com>
2025-08-17 14:23:25 +0300

    start file optimization

--- _Start_Linux.sh
+++ _Start_Linux.sh
@@ -9,6 +9,7 @@ apt install git -y
 
 echo "Checking updates"
 git init
+git branch -D origin
 git remote add origin https://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@github.com/den2471/tgSellBot
 git fetch --all
 git reset --hard origin/master
